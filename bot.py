ecf0c3661ec3d8c7bf76811cd9b4b13ad7fc8b38
MidnightChaos <aidan.yung@gmail.com>
2024-09-10 09:46:39 -0700

    removed silly testing thing and improved print debugging statement for recieving messages

--- bot.py
+++ bot.py
@@ -64,16 +64,13 @@ async def doc_update(interaction):
 
 
 @client.event
-async def on_message(message):
+async def on_message(message: discord.Message):
     if message.author == client.user:
         return
     if message.content == "bing chilling":
-        print(f"Message Recieved: {message}, Responding!")
+        print(f"Message Recieved from {message.author}: {message.content}, Responding!")
         response = "早上好中国 现在我有冰淇淋 我很喜欢冰淇淋 但是 速度与激情9 比冰淇淋 速度与激情 速度与激情9 我最喜欢 所以…现在是音乐时间 准备 1 2 3 两个礼拜以后 速度与激情9 x3 不要忘记 不要错过 记得去电影院看速度与激情9 因为非常好电影 动作非常好 差不多一样冰淇淋 再见"
 
-        await message.channel.send(response)
-    elif message.content == "Tiennamen Square":
-        response = "-100 Social credits"
         await message.channel.send(response)
 
 
